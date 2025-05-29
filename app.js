972df4358f0f5b6964176d3099a728f0644be4ac
smdeath1 <danilseplecov@gmail.com>
2025-05-29 22:57:42 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function showSection(sectionId) {
-    document.querySelectorAll('.section').forEach(el => {
-      el.style.display = 'none';
-    });
-    document.getElementById(sectionId).style.display = 'block';
-  }
-  
-  // Здесь позже будут функции для загрузки уроков, тестов и слов
